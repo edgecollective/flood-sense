33cc4d800ab88f696e952ae75bbd99be4f30d568
Don Blair <donblair@gmail.com>
2020-05-12 17:21:58 -0400

    update

--- circuitpython/gateway/m4_express_airlift/secrets.py
+++
@@ -1,6 +0,0 @@
-secrets = {
-    'ssid' : b'InmanSquareOasis',
-    'password' : b'portauprince',
-    'farmos_pubkey' : 'b0d28215311fc41b2cdfa6d1c09bba83',
-    'farmos_privkey' : '2e65c9e07ff63e9548be632a864a4a52'
-    }
