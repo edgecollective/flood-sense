cd3a24e1058f47bb4cf006b891749e1aaf7bc789
Don Blair <donblair@gmail.com>
2020-05-16 10:07:16 -0400

    update

---
+++ circuitpython/gateway/network_secrets.py
@@ -0,0 +1,6 @@
+network_secrets = {
+    'ssid' : b'InmanSquareOasis',
+    'password' : b'portauprince',
+    'farmos_pubkey' : 'b0d28215311fc41b2cdfa6d1c09bba83',
+    'farmos_privkey' : '2e65c9e07ff63e9548be632a864a4a52'
+    }
